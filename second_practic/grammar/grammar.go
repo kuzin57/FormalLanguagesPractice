47809ed875d05b400e92bed3078a238ddb6c2082
kuzin57 <rkuzin.2003@gmail.com>
2022-11-30 13:13:55 +0300

    removed comments and binary file

--- second_practic/grammar/grammar.go
+++ second_practic/grammar/grammar.go
@@ -6,25 +6,9 @@ func NewGrammar() *Grammar {
 	}
 }
 
-// func newReader(grammar *grammar, word string) *reader {
-// 	return &reader{
-// 		grammar:               grammar,
-// 		currentConfigurations: make([]map[configuration]struct{}, 1),
-// 		word:                  word,
-// 	}
-// }
-
 func (g *Grammar) AddNewStartSymbol() {
 	g.Rules[startSymbol] = []string{"S"}
 }
-
-// func (g *Grammar) CheckWord(word string) bool {
-// 	reader := newReader(g, word)
-// 	reader.initScan()
-// 	reader.initPredict()
-// 	reader.initComplete()
-// 	return reader.read(word)
-// }
 
 func (g *Grammar) AddRule(nt byte, rightPart string) {
 	_, ok := g.Rules[nt]
